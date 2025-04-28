f3b7b143a29d2c3b81824e35ff66e716044761c9
emmakoz15 <emmakozlovsky@gmail.com>
2025-04-28 15:00:24 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+//js for the card carousel
+let swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    grabCursor: true,
+    loop: true,
+    // Pagination
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    // Next and previous navigation
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    // Responsive breakpoints
+    breakpoints: {
+      0: {
+        slidesPerView: 1
+      },
+      768: {
+        slidesPerView: 2
+      },
+      1024: {
+        slidesPerView: 3
+      }
+    }
+  });
+  
