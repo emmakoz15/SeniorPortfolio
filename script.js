5ddc63eb67c32417fea0c727f368862aa6c714c3
emmakoz15 <emmakozlovsky@gmail.com>
2025-05-16 14:57:16 -0400

    started layout for changing cards on web page

--- script.js
+++ script.js
@@ -1,72 +1,71 @@
-//js for the card carousel
-const wrapper = document.querySelector(".wrapper");
-const carousel = document.querySelector(".carousel");
-const firstCardWidth = carousel.querySelector(".card").offsetWidth;
-const arrowBtns = document.querySelectorAll(".wrapper i");
-const carouselChildrens = [...carousel.children];
-let isDragging = false, isAutoPlay = true, startX, startScrollLeft, timeoutId;
-// Get the number of cards that can fit in the carousel at once
-let cardPerView = Math.round(carousel.offsetWidth / firstCardWidth);
-// Insert copies of the last few cards to beginning of carousel for infinite scrolling
-carouselChildrens.slice(-cardPerView).reverse().forEach(card => {
-    carousel.insertAdjacentHTML("afterbegin", card.outerHTML);
+$(document).ready(function () {
+  //changes the  cards for web section
+  //freshman year
+  $("#fresh").click(function () {
+    //card1
+    $("#color1").css("background-color", "blue");
+    $("#textChange1").text("Something from Freshman year");
+    $(".what-image").css("background-image", 'url("images/Ellipse1.png")');
+    //card2
+    $("#color2").css("background-color", "blue");
+    $("#textChange2").text("Something from Freshman year");
+    $(".offer-image").css("background-image", 'url("images/Ellipse1.png")');
+
+    //card3
+    $("#color3").css("background-color", "blue");
+    $("#textChange2").text("Something from Freshman year");
+    $(".staff-image").css("background-image", 'url("images/Ellipse1.png")');
+  });
+//sophomore year
+   $("#soph").click(function () {
+    //card1
+    $("#color1").css("background-color", "green");
+    $("#textChange1").text("Something from Freshman year");
+    $(".what-image").css("background-image", 'url("images/Ellipse2.png")');
+    //card2
+    $("#color2").css("background-color", "green");
+    $("#textChange2").text("Something from Freshman year");
+    $(".offer-image").css("background-image", 'url("images/Ellipse2.png")');
+
+    //card3
+    $("#color3").css("background-color", "green");
+    $("#textChange2").text("Something from Freshman year");
+    $(".staff-image").css("background-image", 'url("images/Ellipse2.png")');
+  });
+
+//junior year
+   $("#jun").click(function () {
+    //card1
+    $("#color1").css("background-color", "red");
+    $("#textChange1").text("Something from Freshman year");
+    $(".what-image").css("background-image", 'url("images/Ellipse3.png")');
+    //card2
+    $("#color2").css("background-color", "red");
+    $("#textChange2").text("Something from Freshman year");
+    $(".offer-image").css("background-image", 'url("images/Ellipse3.png")');
+
+    //card3
+    $("#color3").css("background-color", "red");
+    $("#textChange2").text("Something from Freshman year");
+    $(".staff-image").css("background-image", 'url("images/Ellipse3.png")');
+  });
+
+
+  //senior year
+   $("#sen").click(function () {
+    //card1
+    $("#color1").css("background-color", "gray");
+    $("#textChange1").text("Something from Freshman year");
+    $(".what-image").css("background-image", 'url("images/Ellipse1.png")');
+    //card2
+    $("#color2").css("background-color", "gray");
+    $("#textChange2").text("Something from Freshman year");
+    $(".offer-image").css("background-image", 'url("images/Ellipse1.png")');
+
+    //card3
+    $("#color3").css("background-color", "gray");
+    $("#textChange2").text("Something from Freshman year");
+    $(".staff-image").css("background-image", 'url("images/Ellipse1.png")');
+  });
+
 });
-// Insert copies of the first few cards to end of carousel for infinite scrolling
-carouselChildrens.slice(0, cardPerView).forEach(card => {
-    carousel.insertAdjacentHTML("beforeend", card.outerHTML);
-});
-// Scroll the carousel at appropriate postition to hide first few duplicate cards on Firefox
-carousel.classList.add("no-transition");
-carousel.scrollLeft = carousel.offsetWidth;
-carousel.classList.remove("no-transition");
-// Add event listeners for the arrow buttons to scroll the carousel left and right
-arrowBtns.forEach(btn => {
-    btn.addEventListener("click", () => {
-        carousel.scrollLeft += btn.id == "left" ? -firstCardWidth : firstCardWidth;
-    });
-});
-const dragStart = (e) => {
-    isDragging = true;
-    carousel.classList.add("dragging");
-    // Records the initial cursor and scroll position of the carousel
-    startX = e.pageX;
-    startScrollLeft = carousel.scrollLeft;
-}
-const dragging = (e) => {
-    if(!isDragging) return; // if isDragging is false return from here
-    // Updates the scroll position of the carousel based on the cursor movement
-    carousel.scrollLeft = startScrollLeft - (e.pageX - startX);
-}
-const dragStop = () => {
-    isDragging = false;
-    carousel.classList.remove("dragging");
-}
-const infiniteScroll = () => {
-    // If the carousel is at the beginning, scroll to the end
-    if(carousel.scrollLeft === 0) {
-        carousel.classList.add("no-transition");
-        carousel.scrollLeft = carousel.scrollWidth - (2 * carousel.offsetWidth);
-        carousel.classList.remove("no-transition");
-    }
-    // If the carousel is at the end, scroll to the beginning
-    else if(Math.ceil(carousel.scrollLeft) === carousel.scrollWidth - carousel.offsetWidth) {
-        carousel.classList.add("no-transition");
-        carousel.scrollLeft = carousel.offsetWidth;
-        carousel.classList.remove("no-transition");
-    }
-    // Clear existing timeout & start autoplay if mouse is not hovering over carousel
-    clearTimeout(timeoutId);
-    if(!wrapper.matches(":hover")) autoPlay();
-}
-const autoPlay = () => {
-    if(window.innerWidth < 800 || !isAutoPlay) return; // Return if window is smaller than 800 or isAutoPlay is false
-    // Autoplay the carousel after every 2500 ms
-    timeoutId = setTimeout(() => carousel.scrollLeft += firstCardWidth, 2500);
-}
-autoPlay();
-carousel.addEventListener("mousedown", dragStart);
-carousel.addEventListener("mousemove", dragging);
-document.addEventListener("mouseup", dragStop);
-carousel.addEventListener("scroll", infiniteScroll);
-wrapper.addEventListener("mouseenter", () => clearTimeout(timeoutId));
-wrapper.addEventListener("mouseleave", autoPlay);
